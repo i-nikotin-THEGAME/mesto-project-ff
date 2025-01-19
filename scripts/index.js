adc8b216bfd1b2ddf30020f2f4a2eb9d087f1f40
i-nikotin-THEGAME <i_nikotin@thegame.design>
2025-01-19 18:54:35 +0300

    Проектная работа. Попытка 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cartaTemplate = document.querySelector('#card-template');
 
 // @todo: DOM узлы
+const contenuto = document.querySelector('.content');
+const elencoCarta = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function creareUnaCarta (unoCarta, rimozione) {
+    const nodoCarta = cartaTemplate.content.cloneNode(true);
+    nodoCarta.querySelector('.card__image').src = unoCarta.link;
+    nodoCarta.querySelector('.card__image').alt = unoCarta.name;
+    nodoCarta.querySelector('.card__title').textContent = unoCarta.name;
+    nodoCarta.querySelector('.card__delete-button').addEventListener('click', rimozione);
+    nodoCarta.querySelector('.card__like-button').addEventListener('click', evt => {
+        evt.target.classList.toggle('card__like-button_is-active');
+    });
+    return nodoCarta;
+};
 
 // @todo: Функция удаления карточки
+function rimuovereLaCarta (rimozione) {
+    rimozione.target.parentElement.remove()
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(unoCarta => {
+    elencoCarta.append(creareUnaCarta(unoCarta, rimuovereLaCarta))
+});
